1c37dca79de49221cfdf75a1c1c2b76abbce1cc3
ZhikunWu <598466208@qq.com>
2023-01-06 15:41:01 +0800

    update

--- pipeline/Pan_SV.pipeline.py
+++ pipeline/Pan_SV.pipeline.py
@@ -12,9 +12,9 @@ import yaml
 
 IN_PATH = config['project_path']
 THREADS = config['THREADS']
-threadFold = config['threadFold']
 SAMPLES = config['SAMPLES']
-
+SCAFFOLDS = config['SCAFFOLDS']
+ThreadFold = config['ThreadFold']
 
 # ENV_BIN = config["ENV_BIN"]
 SCRIPT_DIR = config["SCRIPT_DIR"]
@@ -25,15 +25,18 @@ SRC_DIR = config["SRC_DIR"]
 
 # include: RULE_DIR + '/BaseFunction.rule.py'
 include: RULE_DIR + '/QualityControl.rule.py'
-include: RULE_DIR + '/Mapping.rule.py'
-
+# include: RULE_DIR + '/Mapping.rule.py'
+include: RULE_DIR + '/MultiAssembly.rule.py'
 
 
 rule all:
     input:
         ################################# Quality control ############################
-        expand(IN_PATH + '/FastQC/clean/{sample}_stats.xls', sample=SAMPLES),
-        ################################ mapping ####################
-        expand(IN_PATH + '/mapping/{sample}/{sample}_align.bam', sample=SAMPLES),
-        expand(IN_PATH + '/mapping/{sample}/{sample}_bam_stats.xls', sample=SAMPLES),
-
+        # expand(IN_PATH + '/FastQC/clean/{sample}_stats.xls', sample=SAMPLES),
+        # ################################ mapping ####################
+        # expand(IN_PATH + '/mapping/{sample}/{sample}_align.bam', sample=SAMPLES),
+        # expand(IN_PATH + '/mapping/{sample}/{sample}_bam_stats.xls', sample=SAMPLES),
+        # expand(IN_PATH + "/SyRI/{scaffold}/SyRI/{scaffold}_aln_out.txt", scaffold=SCAFFOLDS),
+        expand(IN_PATH + "/clean/{sample}_ONT.fastq.gz", sample=SAMPLES),
+        expand(IN_PATH + '/clean/{sample}.RNA.R1.fq.gz', sample=SAMPLES),
+        expand(IN_PATH + "/Assembly/NextDenovo/{sample}/03.ctg_graph/nd.asm.fasta", sample=SAMPLES),
--- rule/QualityControl.rule.py
+++ rule/QualityControl.rule.py
@@ -1,5 +1,5 @@
 
-######################################################## Quality control  ###########################################################
+######################### DNA Quality control  ####################################################
 rule fastp:
     input:
         R1 = IN_PATH + '/raw/{sample}.html_1.fastq.gz',
@@ -8,7 +8,7 @@ rule fastp:
         R1 = IN_PATH + '/clean/fastp/{sample}.clean.R1.fq.gz',
         R2 = IN_PATH + '/clean/fastp/{sample}.clean.R2.fq.gz',
     threads:
-        THREADS * threadFold
+        THREADS
     params:
         length_required = config["length_required"],
         qualified_quality_phred = config["qualified_quality_phred"],
@@ -67,4 +67,75 @@ rule QCStats:
 
 
 
-#####################################################################################################################################
+##############################################################################################
+
+
+################## RNA quality control #############
+rule fastpRNA:
+    input:
+        R1 = IN_PATH + '/raw/{sample}.RNA.R1.fastq.gz',
+        R2 = IN_PATH + '/raw/{sample}.RNA.R2.fastq.gz',
+    output:
+        R1 = IN_PATH + '/clean/{sample}.RNA.R1.fq.gz',
+        R2 = IN_PATH + '/clean/{sample}.RNA.R2.fq.gz',
+    threads:
+        THREADS
+    params:
+        length_required = config["length_required"],
+        qualified_quality_phred = config["qualified_quality_phred"],
+        unqualified_percent_limit = config["unqualified_percent_limit"],
+        cut_window_size = config["cut_window_size"],
+        cut_mean_quality = config["cut_mean_quality"],
+    log:
+        IN_PATH + "/log/trim/{sample}_fastp.log", 
+    run:
+        ### adapter parameter: --adaptersequence (read1), --adapter_sequence_r2(read2)
+        ### do not trim adapter: --disable_adapter_trimming
+        shell("fastp --in1 {input.R1} --in2 {input.R2} --out1 {output.R1} --out2 {output.R2} --thread {threads} --compression 3 --length_required {params.length_required} --qualified_quality_phred {params.qualified_quality_phred}  --unqualified_percent_limit {params.unqualified_percent_limit} --cut_front --cut_tail --cut_window_size {params.cut_window_size} --cut_mean_quality {params.cut_mean_quality} >{log} 2>&1")
+###################################################
+
+
+
+
+################## Long read quality control ################
+rule NanoQCRaw:
+    input:
+        fastq = IN_PATH + "/raw/{sample}_ONT.fastq.gz",
+    output:
+        temp1 = temp(IN_PATH + "/raw/{sample}_sub.fastq"),
+        qc = IN_PATH + "/QualityControl/raw/NanoQC/{sample}/nanoQC.html",
+    threads:
+        THREADS
+    params:
+        outdir = IN_PATH + "/QualityControl/raw/NanoQC/{sample}",
+    log:
+        IN_PATH + "/log/nanoQC_raw_{sample}.log"
+    run:
+        shell("seqtk sample -s100 {input.fastq} 0.01  > {output.temp1} 2>>{log}")
+        shell("nanoQC --outdir {params.outdir} {output.temp1} 2>>{log}")
+
+
+rule NanoFilt:
+    input:
+        fastq = IN_PATH + "/raw/{sample}_ONT.fastq.gz",
+    output:
+        fastq = IN_PATH + "/clean/{sample}_ONT.fastq.gz",
+    threads:
+        THREADS
+    log:
+        IN_PATH + "/log/NanoFilt_{sample}.log"
+    params:
+        readtype = config["readtype"],
+        minQuality = config["minQuality"],
+        minLength = config["minLength"],
+        headcrop = config["headcrop"],
+        tailcrop = config["tailcrop"],
+    run:
+        # ### Filt out reads of phage lambda
+        # shell("gunzip -c {input.fastq} | NanoLyse --reference {params.phage_lambda} | NanoFilt --quality  {params.minQuality} --length {params.minLength} --headcrop {params.headcrop} --tailcrop {params.tailcrop} | gzip > {output.fastq} 2>{log}")
+        # shell("gunzip -c {input.fastq} | NanoFilt --readtype {params.readtype} --quality  {params.minQuality} --length {params.minLength} --headcrop {params.headcrop} --tailcrop {params.tailcrop} | gzip > {output.fastq} 2>{log}")
+        cmd = "source activate NanoSV &&  gunzip -c %s | NanoFilt --readtype %s --quality  %s --length %s --headcrop %s --tailcrop %s | gzip > %s 2>%s" % (input.fastq, params.readtype, params.minQuality, params.minLength, params.headcrop, params.tailcrop, output.fastq, log)
+        print(cmd)
+        os.system(cmd)
+        # shell("NanoFilt --readtype {params.readtype} --quality  {params.minQuality} --length {params.minLength} --headcrop {params.headcrop} --tailcrop {params.tailcrop} < {input.fastq} | gzip > {output.fastq} 2>{log}")
+################################################################
